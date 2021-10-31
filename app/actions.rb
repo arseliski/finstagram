c601555fdd2654d76e3d7492a2f394fcce24ceb5
arseliski <adam@seliski.ca>
2021-10-31 20:04:30 +0000

    predeployment

--- app/actions.rb
+++ app/actions.rb
@@ -1,56 +1,103 @@
-
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
-
-    end
-
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
 
 get '/' do
-   @finstagram_post_shark = {
-    username: "sharky_j",
-   avatar_url: "http://naserca.com/images/sharky_j.jpg",
-   photo_url: "http:/naserca.com/images/shark.jpg",
-   humanized_time_ago: humanized_time_ago(15),
-   like_count: 0,
-   comment_count: 1,
-   comments: [{
-       username: "sharky_j",
-       text: "Out for the long weekend...too embarrassed to show y'all the beach bod!"
-   }]
-   }
-   
-   @finstagram_post_whale = {
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
 
-   username: "kirk_whalum",
-   avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-   photo_url: "http://naserca.com/images/whale.jpg",
-   humanized_time_ago: humanized_time_ago(65),
-   like_count: 0,
-   comment_count: 1,
-   comments: [{
-       username: "kirk_whalum",
-       text: "#weekendvibes"
-   }]
-   }
-   @finstagram_post_marlin = {
-       username: "marlin_peppa",
-       avatar_url: "http:/naserca.com/images/marlin_peppa.jpg",
-       photo_url: "http://naserca.com/images/marlin.jpg",
-       humanized_time_ago: humanized_time_ago(190),
-       like_count: 0,
-       comment_count: 1,
-       comments: [{
-           username: "marlin_peppa",
-           text: "lunchtime! ;)"
-       }]
-   }
+get '/signup' do     
+  @user = User.new   
+  erb(:signup)       
+end
+
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.find_by(username: username)
+
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    "Success! User with id #{session[:user_id]} is logged in!"
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
 
 
-   @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-   
-    erb :index
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+  erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
+end
+
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  comment.save
+
+  redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
 end
